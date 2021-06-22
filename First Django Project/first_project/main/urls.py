ec922465a7bf288e431d77c2c34e00f8732f4981
Taylor Fancher <tfanch24@gmail.com>
2021-06-21 22:05:04 -0500

    progess on assignment

--- First Django Project/first_project/main/urls.py
+++ First Django Project/first_project/main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('new', views.new),
+    path('create', views.create),
 
 ]
--- First Django Project/first_project/main/views.py
+++ First Django Project/first_project/main/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.http.response import HttpResponsePermanentRedirect
+from django.shortcuts import redirect, render, HttpResponse
 
 def index(response):
     return HttpResponse('placeholder to later display a list of all blogs')
+
+def new(response):
+    return HttpResponse('placeholder to display a new form to create a new blog')
+
+def create(response):
+    return redirect('/')
+
 # Create your views here.
