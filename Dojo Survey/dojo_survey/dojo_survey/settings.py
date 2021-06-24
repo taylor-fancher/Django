28d2d64f28fb0da948ed77742a7c3d1dc0a45189
Taylor Fancher <tfanch24@gmail.com>
2021-06-23 19:18:40 -0500

    added gitignore

--- Dojo Survey/dojo_survey/dojo_survey/settings.py
+++ Dojo Survey/dojo_survey/dojo_survey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wg48r4lx=!s^a1mn+@pcsgl=7$8gpa+$v6yvk$*2qt-#$w!%kq'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
