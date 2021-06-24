e110d148008c30e324d72ee55d46e659265f82fc
Taylor Fancher <tfanch24@gmail.com>
2021-06-24 10:12:16 -0500

    progress on form

--- Dojo Survey/dojo_survey/main/views.py
+++ Dojo Survey/dojo_survey/main/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def index(response):
-    pass
+def index(request):
+    return render(request, 'index.html')
+
+def submit(request):
+    request.session['name'] = request.POST['name']
+    request.session['location'] = request.POST['location']
+    request.sessiong['language'] = request.POST['language']
+    return redirect('/results')
+
+def results(request):
+    context = {
+        'name': request.session['name'],
+        'location' : request.session['location'],
+        'language' : request.session['language']
+    }
+    return render(request, 'results.html', context)
 # Create your views here.
