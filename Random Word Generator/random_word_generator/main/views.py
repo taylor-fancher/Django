a8a526033394606779b59d3c55d2de5530e64edf
Taylor Fancher <tfanch24@gmail.com>
2021-06-26 22:09:21 -0500

    set up basic app

--- Random Word Generator/random_word_generator/main/views.py
+++ Random Word Generator/random_word_generator/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_random_string
 
+def index(request):
+    return render(response, 'index.html')
+
+def random(request):
+    request.session
 # Create your views here.
