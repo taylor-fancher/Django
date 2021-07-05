3732e20fa3b337db825b311000c97e851847035b
Taylor Fancher <tfanch24@gmail.com>
2021-07-05 08:06:34 -0500

    finished assignment

--- Users_Shell/single_model_orm/users/views.py
+++ Users_Shell/single_model_orm/users/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
+from .models import User
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'users': User.objects.all
+    }
+    return render(request, 'index.html', context)
 
+def create(request):
+    user = User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email_address = request.POST['email_address'],
+        age = request.POST['age']
+    )
+    return redirect('/')
 # Create your views here.
