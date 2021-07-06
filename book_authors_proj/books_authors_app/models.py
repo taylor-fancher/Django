0b0fbde5c46ed99566237ff133c6d7e2057510be
Taylor Fancher <tfanch24@gmail.com>
2021-07-05 20:21:09 -0500

    finished assignment

--- book_authors_proj/books_authors_app/models.py
+++ book_authors_proj/books_authors_app/models.py
@@ -10,6 +10,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     notes = models.TextField()
+    books = models.ManyToManyField(Book, related_name='authors')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
