985af36240ae83fc60a514833e72a0066bc6bb8e
Taylor Fancher <tfanch24@gmail.com>
2021-07-06 08:01:49 -0500

    Setup Templates

--- book_authors_proj/books_authors_app/urls.py
+++ book_authors_proj/books_authors_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('books', views.books),
+    path('authors', views.authors),
+    path('create_book', views.create_book),
+    path('book/<int:number>', views.this_book)
 ]
--- book_authors_proj/books_authors_app/views.py
+++ book_authors_proj/books_authors_app/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author
 
 def index(request):
-    pass
+    return render(request, 'index.html')
+
+def books(request):
+    context = {
+        'books': Book.objects.all
+    }
+    return render(request, 'books.html', context)
+
+def create_book(request):
+    book = Book.objects.create(
+        title = request.POST['title'],
+        desc = request.POST['desc']
+    )
+    return redirect ('/books')
+
+def this_book(request, number):
+    context = {
+        'books': Book.objects.all
+    }
+    return render(request, 'this_book.html', context)
+
+def authors(request):
+    return render(request, 'authors.html')
 
 # Create your views here.
